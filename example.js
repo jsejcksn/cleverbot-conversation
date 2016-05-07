b59cb370c8b78f1cf63e603c4cf11ffb5221cba0
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-07 09:01:47 -0500

    Add example

---
+++ example.js
@@ -0,0 +1,20 @@
+var api = require('./api.js');
+var cleverbot = require("cleverbot.io");
+var readline = require('readline');
+var rl = readline.createInterface(process.stdin, process.stdout);
+bot = new cleverbot(api.user, api.key); // cleverbot (user, key, nick)
+
+// bot.setNick("YOUR_NICK"); // This is optional
+bot.create(function (err, response) {
+	rl.setPrompt('You> ');
+	rl.prompt();
+	rl.on('line', function(line) {
+		bot.ask(line, function (err, response) {
+			if (err) throw response;
+			console.log("Cleverbot:", response);
+			rl.prompt();
+		});
+	}).on('close',function(){
+		process.exit(0);
+	});
+});

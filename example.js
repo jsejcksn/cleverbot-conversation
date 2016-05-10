a43fde5b3752bb7450f8dfa9af451cdd8093377d
Jesse Jackson <jsejcksn@users.noreply.github.com>
2016-05-10 02:12:10 -0500

    Clarify variables, cleanup indent

--- example.js
+++ example.js
@@ -5,16 +5,16 @@ var rl = readline.createInterface(process.stdin, process.stdout);
 bot = new cleverbot(api.user, api.key); // cleverbot (user, key, nick)
 
 // bot.setNick("YOUR_NICK"); // This is optional
-bot.create(function (err, response) {
-	rl.setPrompt('You> ');
-	rl.prompt();
-	rl.on('line', function(line) {
-		bot.ask(line, function (err, response) {
-			if (err) throw response;
-			console.log("Cleverbot:", response);
-			rl.prompt();
-		});
-	}).on('close',function(){
-		process.exit(0);
-	});
+bot.create(function(err, response) {
+  rl.setPrompt('You> ');
+  rl.prompt();
+  rl.on('line', function(input) {
+    bot.ask(input, function(err, response) {
+      if (err) throw response;
+      console.log("Cleverbot:", response);
+      rl.prompt();
+    });
+  }).on('close', function() {
+    process.exit(0);
+  });
 });
